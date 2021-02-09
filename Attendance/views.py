73cd53322d5aa264599fbdc823671c988e24fe8d
peter4ease <peter.edoka.242464@unn.edu.ng>
2021-02-09 20:29:25 +0100

    modified the upload dir 2

--- Attendance/views.py
+++ Attendance/views.py
@@ -306,7 +306,8 @@ class FinalView(LoginRequiredMixin, View):
         mass_list = Mass.objects.all()
         sheet_title = datetime.date.today().strftime('%d-%m-%Y')
 
-        wb_path = 'media_cdn/media/log/' + sheet_title + '.xlsx'
+        sheet_dir = sheet_title + '.xlsx'
+        wb_path = sheet_dir
         wb = load_workbook(wb_path)
 
         if sheet_title in wb.sheetnames:
@@ -341,7 +342,7 @@ class FinalView(LoginRequiredMixin, View):
 
         try:
             wb.save(wb_path)
-            LogsFile.objects.create(adminupload='media/log/' + sheet_title + '.xlsx', title=sheet_title, total_members=This_Sunday_Member.objects.count())
+            LogsFile.objects.create(adminupload=sheet_dir, title=sheet_title, total_members=This_Sunday_Member.objects.count())
             This_Sunday_Member.objects.all().delete()
 
         except:
